be9a03c160f0074c0d7748ad7b5c8d9e3ff9df52
kenneth <joshuabecaro@gmail.com>
2026-03-25 02:38:08 +0800

    (frontend) feat: main comopnents

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import { FeedContainer } from "./features/feed/components/FeedContainer";
+
+function App() {
+  return (
+    <div className="w-full h-screen  overflow-hidden m-0 p-0 text-white font-sans sm:max-w-md sm:mx-auto">
+      <FeedContainer />
+    </div>
+  );
+}
+
+export default App;
